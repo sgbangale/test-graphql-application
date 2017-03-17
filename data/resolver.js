586876533100f1e4948b8712d3d2e7aea3be94f9
suraj <sgbangale@gmail.com>
2017-03-17 10:23:51 +0000

    added graphql custom types -person

--- data/resolver.js
+++ data/resolver.js
@@ -1,11 +1,18 @@
+var casual = require("casual")
 var resolver = {
   hello: () => {
     return 'Hello world!';
   },
   
-  dice :(_,args,v)=>{
+  dice :(args)=>{
       return "this is output " +args.left +"  " + args.right;
   },
+  
+  person : (args)=>{
+      
+      return  {name : args.name,age : casual.integer(from = 20, to = 60) , gender : "Male",citylived: casual.city};
+  }
+  
   };
   
 
--- data/schema.js
+++ data/schema.js
@@ -5,6 +5,16 @@ var schema =new buildSchema.buildSchema (`
   type Query {
     hello: String
     dice(left: Int,right : String) : String
+    person(name: String) : Person
+  }
+  
+  type Person
+  {
+    name: String!
+    age: Int
+    gender: String
+    citylived: String
+    
   }
 `);
 
